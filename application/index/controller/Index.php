010b49b45014df6d557372b737874b1805bc7670
liuchao <1778316892@qq.com>
2020-03-31 13:28:34 +0800

    新增queryList插件

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,173 +2,191 @@
 namespace app\index\controller;
 use think\Controller;
 use think\Db;
+use QL\QueryList;
 
 class Index extends Controller
 {
 
-    function shuangseqiu(){
-        $date = date('Y-m-d');
-        $refresh = input('refresh', 0, 'intval');
-        $model = Db::name('shuangseqiu');
-        if(!$refresh && $res = $model->where('date', $date)->find()){
-            $nums = [
-                $res['num1'],
-                $res['num2'],
-                $res['num3'],
-                $res['num4'],
-                $res['num5'],
-                $res['num6'],
-                $res['num7'],
-            ];
-        }else{
-            $red_max = 33;
-            $blue_max = 16;
-            $red = [];
-            $blue = [];
-            for($i=1;$i<=6;$i++){
-                $red_flag = true;
-                while ($red_flag) {
-                    $num = rand(1, $red_max);
-                    if(!in_array($num, $red)){
-                        $red[] = $num;
-                        $red_flag = false;
-                    }
-                }
-            }
-            sort($red);
+	function shuangseqiu(){
+	   $date = date('Y-m-d');
+		$refresh = input('refresh', 0, 'intval');
+		$model = Db::name('shuangseqiu');
+		if(!$refresh && $res = $model->where('date', $date)->find()){
+			$nums = [
+				$res['num1'],
+				$res['num2'],
+				$res['num3'],
+				$res['num4'],
+				$res['num5'],
+				$res['num6'],
+				$res['num7'],
+			];
+		}else{
+			$red_max = 33;
+			$blue_max = 16;
+			$red = [];
+			$blue = [];
+			for($i=1;$i<=6;$i++){
+				$red_flag = true;
+				while ($red_flag) {
+					$num = rand(1, $red_max);
+					if(!in_array($num, $red)){
+						$red[] = $num;
+						$red_flag = false;
+					}
+				}
+			}
+			sort($red);
 
-            $blue_flag = true;
-            while ($blue_flag) {
-                $num = rand(1, $blue_max);
-                if(!in_array($num, $blue)){
-                    $blue[] = $num;
-                    $blue_flag = false;
-                }
-            }
-            $nums = [
-                $red[0],
-                $red[1],
-                $red[2],
-                $red[3],
-                $red[4],
-                $red[5],
-                $blue[0],
-            ];
+			$blue_flag = true;
+			while ($blue_flag) {
+				$num = rand(1, $blue_max);
+				if(!in_array($num, $blue)){
+					$blue[] = $num;
+					$blue_flag = false;
+				}
+			}
+			$nums = [
+				$red[0],
+				$red[1],
+				$red[2],
+				$red[3],
+				$red[4],
+				$red[5],
+				$blue[0],
+			];
 
-            if($model->where('date', $date)->count() > 0){
-                $model->where('date', $date)->update([
-                    'date' => $date,
-                    'num1' => $red[0],
-                    'num2' => $red[1],
-                    'num3' => $red[2],
-                    'num4' => $red[3],
-                    'num5' => $red[4],
-                    'num6' => $red[5],
-                    'num7' => $blue[0],
-                ]);
-            }else{
-                $model->insert([
-                    'date' => $date,
-                    'num1' => $red[0],
-                    'num2' => $red[1],
-                    'num3' => $red[2],
-                    'num4' => $red[3],
-                    'num5' => $red[4],
-                    'num6' => $red[5],
-                    'num7' => $blue[0],
-                ]);
-            }
-            if($refresh){
-                return $nums;
-            }
-        }
+			if($model->where('date', $date)->count() > 0){
+				$model->where('date', $date)->update([
+					'date' => $date,
+					'num1' => $red[0],
+					'num2' => $red[1],
+					'num3' => $red[2],
+					'num4' => $red[3],
+					'num5' => $red[4],
+					'num6' => $red[5],
+					'num7' => $blue[0],
+				]);
+			}else{
+				$model->insert([
+					'date' => $date,
+					'num1' => $red[0],
+					'num2' => $red[1],
+					'num3' => $red[2],
+					'num4' => $red[3],
+					'num5' => $red[4],
+					'num6' => $red[5],
+					'num7' => $blue[0],
+				]);
+			}
+			if($refresh){
+				return $nums;
+			}
+		}
 
-        $this->assign('nums', $nums);
-        return view();
-    }
+		$this->assign('nums', $nums);
+		return view();
+	}
 
-    function daletou(){
-        $date = date('Y-m-d');
-        $refresh = input('refresh', 0, 'intval');
-        $model = Db::name('daletou');
-        if(!$refresh && $res = $model->where('date', $date)->find()){
-            $nums = [
-                $res['num1'],
-                $res['num2'],
-                $res['num3'],
-                $res['num4'],
-                $res['num5'],
-                $res['num6'],
-                $res['num7'],
-            ];
-        }else{
-            $red_max = 35;
-            $blue_max = 12;
-            $red = [];
-            $blue = [];
-            for($i=1;$i<=5;$i++){
-                $red_flag = true;
-                while ($red_flag) {
-                    $num = rand(1, $red_max);
-                    if(!in_array($num, $red)){
-                        $red[] = $num;
-                        $red_flag = false;
-                    }
-                }
-            }
-            sort($red);
+	function daletou(){
+		$date = date('Y-m-d');
+		$refresh = input('refresh', 0, 'intval');
+		$model = Db::name('daletou');
+		if(!$refresh && $res = $model->where('date', $date)->find()){
+			$nums = [
+				$res['num1'],
+				$res['num2'],
+				$res['num3'],
+				$res['num4'],
+				$res['num5'],
+				$res['num6'],
+				$res['num7'],
+			];
+		}else{
+			$red_max = 35;
+			$blue_max = 12;
+			$red = [];
+			$blue = [];
+			for($i=1;$i<=5;$i++){
+				$red_flag = true;
+				while ($red_flag) {
+					$num = rand(1, $red_max);
+					if(!in_array($num, $red)){
+						$red[] = $num;
+						$red_flag = false;
+					}
+				}
+			}
+			sort($red);
 
-            for($i=1;$i<=2;$i++){
-            	$blue_flag = true;
-	            while ($blue_flag) {
-	                $num = rand(1, $blue_max);
-	                if(!in_array($num, $blue)){
-	                    $blue[] = $num;
-	                    $blue_flag = false;
-	                }
-	            }
-            }
-            sort($blue);
+			for($i=1;$i<=2;$i++){
+				$blue_flag = true;
+				while ($blue_flag) {
+					$num = rand(1, $blue_max);
+					if(!in_array($num, $blue)){
+						$blue[] = $num;
+						$blue_flag = false;
+					}
+				}
+			}
+			sort($blue);
 
-            $nums = [
-                $red[0],
-                $red[1],
-                $red[2],
-                $red[3],
-                $red[4],
-                $blue[0],
-                $blue[1],
-            ];
+			$nums = [
+				$red[0],
+				$red[1],
+				$red[2],
+				$red[3],
+				$red[4],
+				$blue[0],
+				$blue[1],
+			];
 
-            if($model->where('date', $date)->count() > 0){
-                $model->where('date', $date)->update([
-                    'date' => $date,
-                    'num1' => $red[0],
-                    'num2' => $red[1],
-                    'num3' => $red[2],
-                    'num4' => $red[3],
-                    'num5' => $red[4],
-                    'num6' => $blue[0],
-                    'num7' => $blue[1],
-                ]);
-            }else{
-                $model->insert([
-                    'date' => $date,
-                    'num1' => $red[0],
-                    'num2' => $red[1],
-                    'num3' => $red[2],
-                    'num4' => $red[3],
-                    'num5' => $red[4],
-                    'num6' => $blue[0],
-                    'num7' => $blue[1],
-                ]);
-            }
-            if($refresh){
-                return $nums;
-            }
-        }
+			if($model->where('date', $date)->count() > 0){
+				$model->where('date', $date)->update([
+					'date' => $date,
+					'num1' => $red[0],
+					'num2' => $red[1],
+					'num3' => $red[2],
+					'num4' => $red[3],
+					'num5' => $red[4],
+					'num6' => $blue[0],
+					'num7' => $blue[1],
+				]);
+			}else{
+				$model->insert([
+					'date' => $date,
+					'num1' => $red[0],
+					'num2' => $red[1],
+					'num3' => $red[2],
+					'num4' => $red[3],
+					'num5' => $red[4],
+					'num6' => $blue[0],
+					'num7' => $blue[1],
+				]);
+			}
+			if($refresh){
+				return $nums;
+			}
+		}
 
-        $this->assign('nums', $nums);
-        return view();
-    }
+		$this->assign('nums', $nums);
+		return view();
+	}
+
+	function daletou_history(){
+		$url = 'http://datachart.500.com/dlt/history/history.shtml';
+		// 定义采集规则
+		$rules = [
+			// 采集期号
+			'qi' => ['#tdata .t_tr1>.t_tr1','text'],
+		];
+		$table = QueryList::get($url)->find('#tablelist');
+
+		// 采集表的每行内容
+		$tableRows = $table->find('tr:gt(0)')->map(function($row){
+		    return $row->find('td')->texts()->all();
+		});
+
+		dump($tableRows->all());
+	}
 }
